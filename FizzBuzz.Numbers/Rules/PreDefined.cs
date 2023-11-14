4e1e546a1039a754490cf1b869daeb76a6faa755
André Biseth <andre@biseth.net>
2023-11-14 13:40:51 +0000

    Added new rule Hazz

--- FizzBuzz.Numbers/Rules/PreDefined.cs
+++ FizzBuzz.Numbers/Rules/PreDefined.cs
@@ -4,3 +4,4 @@ public sealed record Fizz() : Rule(3, nameof(Fizz));
 public sealed record Buzz() : Rule(5, nameof(Buzz));
 public sealed record Jazz() : Rule(9, nameof(Jazz));
 public sealed record Fuzz() : Rule(4, nameof(Fuzz));
+public sealed record Hazz() : Rule(12, nameof(Hazz));
